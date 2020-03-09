f8abb2a0ae471cfdc14b514f60373013f0691b31
leesue630 <lee.sue630@gmail.com>
2020-03-09 16:10:46 -0400

    edited App.js to conform with FrameView

--- src/App.js
+++ src/App.js
@@ -1,37 +1,80 @@
-import React from 'react';
-import Panels from './Panels';
-import Header from './builtin/Header';
-import ColorSelector from './builtin/ColorSelector';
+import React from "react";
+import Panels from "./Panels";
+import Header from "./builtin/Header";
+import ColorSelector from "./builtin/ColorSelector";
 
 export default function App() {
-  const [selectedIndex, setIndex] = React.useState(0);
-  const [colors, setColors] = React.useState({
-    0: "#808080", 
-    1: "#808080", 
-    2: "#808080",
-    3: "#808080",
-    4: "#808080",
-    5: "#808080",
-    6: "#808080",
-    7: "#808080",
-    8: "#808080",
+  const defaultFrame = {
+    colors: {
+      0: "#808080",
+      1: "#808080",
+      2: "#808080",
+      3: "#808080",
+      4: "#808080",
+      5: "#808080",
+      6: "#808080",
+      7: "#808080",
+      8: "#808080"
+    },
+    duration: 1
+  };
+  const [selectedPanelIndex, setPanelIndex] = React.useState(0);
+  const [selectedFrameIndex, setFrameIndex] = React.useState(0);
+  const [frames, setFrames] = React.useState({
+    0: Object.assign({}, defaultFrame) // clones defaultFrame object
   });
+  const [frameCount, setFrameCount] = React.useState(1);
 
-  function selectPanel(newIndex) {
-    setIndex(newIndex);
+  function selectPanel(newPanelIndex) {
+    setPanelIndex(newPanelIndex);
+    console.log("Panel: " + newPanelIndex);
+  }
+
+  function selectFrame(newFrameIndex) {
+    if (newFrameIndex < frameCount) {
+      setFrameIndex(newFrameIndex);
+      console.log("Frame: " + newFrameIndex);
+      setPanelIndex(0);
+    } else {
+      console.log("Out of bounds frame index.");
+    }
+  }
+
+  function addFrame() {
+    var oldSize = frameCount;
+    setFrames({
+      ...frames,
+      [oldSize]: Object.assign({}, defaultFrame),
+    });
+    setFrameCount(oldSize + 1);
+    console.log("Frame added. Frames size: " + frames.size);
+    setFrameIndex(oldSize);
+    selectPanel(0);
   }
 
   function updateColor(newColor) {
-    setColors({ ...colors, [selectedIndex]: newColor });
+    setFrames({ ...frames, [selectedFrameIndex]: { ...colors, [selectedPanelIndex]: newColor} });
+    console.log("Frame " + selectedFrameIndex + ", Panel " + selectedPanelIndex + " color changed");
   }
 
   return (
     <div>
-      <Header name='Srinu Lade' colors={colors} />
-      <Panels colors={Object.values(colors)} selectedIndex={selectedIndex} 
-        selectPanel={selectPanel} />
-      <ColorSelector selectedColor={colors[selectedIndex]} 
-        updateColor={updateColor} />
+      <Header name="Srinu Lade" frames={frames} />
+      <Panels
+        colors={Object.values(frames[selectedFrameIndex].colors)}
+        selectedPanelIndex={selectedPanelIndex}
+        selectPanel={selectPanel}
+      />
+      <ColorSelector
+        selectedColor={frames[selectedFrameIndex].colors[selectedIndex]}
+        updateColor={updateColor}
+      />
+      <FrameView
+        frames={Object.values(frames)}
+        addFrame={addFrame}
+        selectedFrameIndex={selectedFrameIndex}
+        selectFrame={selectFrame}
+      />
     </div>
   );
 }
--- src/frame-components/FrameView.js
+++ src/frame-components/FrameView.js
@@ -1,25 +1,28 @@
 import React from "react";
 import Frame from "./Frame";
+import { Button } from "@material-ui/core";
 
 export default function FrameView(props) {
-
   return (
     <div id="frame-view">
-      {props.frames.map((colors, duration, i) => (
+      {props.frames.map((frame, i) => (
         <Frame
           key={i}
           index={i}
-          duration={duration}
-          colors={colors}
-          selectedFrameIndex={props.selectedFrameIndex === i}
+          duration={frame.duration}
+          colors={frame.colors}
+          isSelected={props.selectedFrameIndex === i}
           selectFrame={props.selectFrame}
         />
       ))}
-      <button
-        onClick={() => this.props.addFrame()}
+      <Button
+        color="inherit"
+        onClick={function() {
+          props.addFrame();
+        }}
       >
-        Add Frame +
-      </button>
+        Add Frame
+      </Button>
     </div>
   );
 }
