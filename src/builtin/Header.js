f883e5708326936bb9c453b769236ea1849764a6
ritatzhang <55301871+ritatzhang@users.noreply.github.com>
2020-05-19 14:40:27 -0400

    added status to header bar

--- src/builtin/Header.js
+++ src/builtin/Header.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-  flex: {flex: 1},
+  flex: { flex: 1 }
 });
 
-const Header = (props) => {
-
+const Header = props => {
   /*
   props = {
     name: 'Srinu Lade',
@@ -20,30 +19,35 @@ const Header = (props) => {
   */
 
   const classes = useStyles();
+  ans = "Nothing Sent Yet";
 
   const sendTheme = async () => {
-    const res = await fetch('http://pbridge.adm.cs.cmu.edu:5000/theme', {
-      method: 'POST',
+    ans = "Pending";
+    const res = await fetch("http://pbridge.adm.cs.cmu.edu:5000/theme", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({ name: props.name, panels: props.colors })
     });
 
-    const ans = await res.json();
+    ans = await res.json();
     console.log(ans);
   };
 
   return (
-    <AppBar position="static" style={{marginBottom : 30}}>
+    <AppBar position="static" style={{ marginBottom: 30 }}>
       <Toolbar>
         <Typography variant="h6" color="inherit" className={classes.flex}>
-            Bridge UI Editor - {props.name}
+          Bridge UI Editor - {props.name}
         </Typography>
-        <Button color="inherit" onClick={sendTheme}>Send Theme</Button>
+        <Button color="inherit">Status: {ans}</Button>
+        <Button color="inherit" onClick={sendTheme}>
+          Send Theme
+        </Button>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default Header;
