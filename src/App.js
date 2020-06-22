a9227db2f16606a3a7ef08678846069d6389d128
leesue630 <lee.sue630@gmail.com>
2020-06-22 18:36:36 -0400

    starter code for basics section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 
-// Components
-import Panels from "./components/Panels";
-
 class App extends Component {
   render() {
     return (
       <div>
-        <Panel color='blue' isSelected={true}></Panel>
+        Do you know the muffin man?
       </div>
     );
   }
