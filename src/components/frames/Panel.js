554e027fb79cc8a2beea519cd91618a0426df444
Shannon Ding <shannon_ding@yahoo.com>
2020-09-05 05:50:29 -0400

    wowza it looks like at least basic and intermediate work

--- src/components/frames/Panel.js
+++ src/components/frames/Panel.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // assets
-import img from "../assets/pausch-design.jpg";
+import img from "../../assets/pausch-design.jpg";
 
 export default function Panel(props) {
   /*
